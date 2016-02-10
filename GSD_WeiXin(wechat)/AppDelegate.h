1dcb838abe1c9efb5ad08c67b62a036aad832316
GSD_iOS <gsdios@126.com>
2016-02-10 17:13:45 +0800

    Initial Commit

---
+++ GSD_WeiXin(wechat)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GSD_WeiXin(wechat)
+//
+//  Created by aier on 16/2/10.
+//  Copyright © 2016年 GSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
